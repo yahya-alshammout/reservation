dfe5be353922fbcfcc4bccf9bea14c3685b2f421
yahay-hilal-alshammout <yhfmsh@mail.ru>
2018-06-23 01:26:03 +0300

    edit customer

--- hotel_system/controllers/customer.py
+++ hotel_system/controllers/customer.py
@@ -3,7 +3,6 @@ class Customer():
 	def __init__(self, customer_name):
 		self.customer_name = customer_name
 	def add_customer(self):
-		for customer in Customer.customers:
 			if [customer_name] in Customer.customers:
 				return None
 			
